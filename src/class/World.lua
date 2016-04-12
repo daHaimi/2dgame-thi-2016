b791651a77a88e58e079eacd768bc79ff570aa39
Marco Egner <mae3129@thi.de>
2016-04-12 20:32:14 +0200

    Some changes

--- src/class/World.lua
+++ src/class/World.lua
@@ -3,26 +3,42 @@
 -- @param windowDimensions The dimensions of the window
 local World = Class{
     init = function(self, backgroundPath, windowDimensions)
-        self.background = backgroundPath;
+        self.background:setWrap("repeat", "repeat");
+        self.backgroundPath = backgroundPath;
+        self.background = love.graphics.newImage(backgroundPath);
         self.windowDimensions = windowDimensions;
-        self.curLevel = Level(love.graphics.newImage("backgroundPath"), windowDimensions);
+        self.posY = windowDimensions[2] / 2;
+        self.bgq = love.graphics.newQuad(0, 0, windowDimensions[1], 20000, background:getWidth(), background:getHeight());
     end;
-    curLevel = Level(love.graphics.setBackgroundColor(255, 255, 255), windowDimensions);
     windowDimensions = {};
-    lowerBoarder = 99999 -- Math.inf; -- only for the beginning! Change this to a balanced value.
+    lowerBoarder = 99999; -- Math.inf; -- only for the beginning! Change this to a balanced value.
     upperBoarder = 0;
+    bgq = nil;
+    background = nil; --love.graphics.
+    posY = 0;
+    direction = 1;
 };
+
+--
+function World:update(dt, bait)
+    if self.direction == 1 then
+        self.sizeY = self.windowDimensions[2] + math.ceil(dt * bait.speed);
+        self.posY = self.posY - math.ceil(dt * bait.speed);
+    end
+end
+
+function World:draw(bait)
+    love.graphics.setColor(255, 255, 255);
+    love.graphics.draw(self.background, self.bgq, 0, self.posY);
+    bait:draw();
+end
+
 
 -- Set a new background image for the world
 -- @param backgroundPath The relative path to the picture
 function World:changeImage(backgroundPath)
-    self.curLevel = Level(love.graphics.newImage("backgroundPath"), windowDimensions);
-end;
-
--- Returns the love level object.
--- @return Returns the love level object.
-function World:getCurLevel()
-    return self.curLevel;
+    self.backgroundPath = backgroundPath;
+    self.background = love.graphics.newImage(backgroundPath);
 end;
 
 -- Set the value for the lower boarder
