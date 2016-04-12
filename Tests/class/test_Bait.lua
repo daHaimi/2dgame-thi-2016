446f5762a4128424fbd36b0e76a54b945c31adde
WamboOSX <barisuk@gmx.net>
2016-04-12 16:54:39 +0200

    created test_Bait with constructor test

---
+++ Tests/class/test_Bait.lua
@@ -0,0 +1,25 @@
+-- Lua 5.1 Hack
+_G.math.inf = 1/0
+
+testClass = require "Bait"
+
+
+describe("Unit test for Bait.lua", function()
+    local locInstance = nil;
+    --- possible Window size
+    local locWinDim = {400, 800}; 
+    
+    before_each(function()
+        locInstance = testClass(locWinDim);
+    end
+    
+     it("Testing Constructor", function()
+        local myInstance = testClass(locWinDim);
+        assert.are.same(locInstance, myInstance);
+    end)
+    
+
+
+
+    
+end)
--- Tests/class/test_Level.lua
+++ Tests/class/test_Level.lua
@@ -1,11 +1,10 @@
 -- Lua 5.1 Hack
 _G.math.inf = 1/0
 
-testClass = require "main"
+testClass = require "Level"
 
-describe("Unit test for main.lua", function()
+describe("Unit test for Level.lua", function()
     it("Example unit test", function()
       assert.truthy("Yup.")
     end)
 end)
-
