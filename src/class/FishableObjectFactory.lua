d8dea96dd5bf869d4236e866dc9795f746abaf47
if1788 <if1788@thi.de>
2016-04-19 17:08:00 +0200

    modified FishableObjectFactory

--- src/class/FishableObjectFactory.lua
+++ src/class/FishableObjectFactory.lua
@@ -4,18 +4,20 @@ local FishableObjectFactory = Class {
     end;
 };
 
---- Creates a new fishable object
--- @param entityType The type of the entity to create
--- @param speed The speed of the entity
--- @param spawnX The x coordinate of the spawn point
--- @param spawnY The y coordinate of the spawn point
--- @param direction The moving direction of the entity
--- @param healthPoints The health points of the entity
--- @param money The value of the entity
-function FishableObjectFactory:create(entityType, speed, spawnX, spawnY, direction, healthPoints, money)
-    if     entityType == "angler"    then print("creating angler fish");
-    elseif entityType == "crocodile" then print("creating crocodile");
-    else                                  error("Unknown entity type");
+--- Create a new FishableObject of type fObjectType
+-- @param fObjectType The type of the object
+-- @param yPosition Height of the object in the level
+function FishableObjectFactory:create(fObjectType, yPosition)
+    if fObjectType == "nemo"    
+        then print("creating nemo");
+    elseif fObjectType == "turtle"  
+        then print("creating turtle");
+    elseif fObjectType == "rat"     
+        then print("creating rat");
+    elseif fObjectType == "deadFish" 
+        then print("creating deadFish");
+    else                                  
+        error("Unknown fishable object type");
     end
 end
 
