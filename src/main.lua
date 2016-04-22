390a18f87bae8a8c1cc13bdc634daf931e096f81
Marco Egner <mae3129@thi.de>
2016-04-22 12:55:02 +0200

    godMode comment added

--- src/main.lua
+++ src/main.lua
@@ -21,7 +21,7 @@ _G._persTable.upgrades = {
     speedUp = 0; --- "0" no Speedup for more looke bait.lua
     moneyMult = 0; --- "0" means no additional money
     moreLife = 0; --- amount of additional lifes
-    godMode = 0; --- 
+    godMode = 0; --- indicates if the god mode is available or not
     mapBreakthrough1 = 0; --- can you access the first map limit? 0 = no, 1 = yes
     mapBreakthrough2 = 0; --- can you access the second map limit? 0 = no, 1 = yes
   };
