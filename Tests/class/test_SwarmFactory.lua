89afbc6d31cb67096927eb1b52c0a87f451beb15
if1788 <if1788@thi.de>
2016-04-22 18:13:16 +0200

    unit tests

---
+++ Tests/class/test_SwarmFactory.lua
@@ -0,0 +1,70 @@
+testClass = require "src.class.SwarmFactory"
+
+describe("Unit test for SwarmFactory.lua", function()
+    local locInstance;
+    
+    before_each(function()
+        _G.love = {
+            graphics = {
+                setColor = function(...) end;
+                newImage = function(...) end;
+                draw = function(...) end;
+                scale = function(...) end;
+            }
+        }
+        
+        _G._persTable = { 
+            winDim = {500; 500};
+            moved = 0;
+        }
+        
+        locInstance = testClass();
+    end)
+
+    it("Testing constructor", function()
+        local myInstance = testClass();
+        assert.are.same(locInstance, myInstance);
+    end)
+
+    it("Testing draw method", function()
+        local loveMock = mock(_G.love, true);
+        locInstance:draw();
+        assert.spy(loveMock.graphics.draw).was.called(#locInstance.createdFishables);
+    end)
+
+    it("Testing update method", function()
+        local s = spy.on(FishableObject, "update");
+        locInstance:update();
+        assert.spy(s).was.called(#locInstance.createdFishables);
+    end)
+
+    it("Testing createNextSwarm method", function()
+        local testInstance = testClass();
+        testInstance.currentSwarm = 0;
+        testInstance.createdFishables = {};
+        testInstance:createNextSwarm(100);
+        testSwarmheight = testInstance.swarmsSewer[testInstance.currentSwarm].swarmHeight;
+        testSwarmMaxYPos = 100 + testSwarmheight;
+        swarm = testInstance.createdFishables;
+        
+        for i = 1, #swarm, 1 do
+            assert.has_no.errors(function() 
+                if swarm[i].yPosition < 100 or swarm[i].yPosition > testSwarmMaxYPos then
+                    error("Position error");
+                end
+            end)
+        end
+    end)
+
+    it("Testing determineFishable method", function()
+        allowedFishablesTest = { "nemo", "lollipop", "deadFish" };
+        fishablesProbabilityTest = { 100, 0, 0 };
+        fishableRes = locInstance:determineFishable(allowedFishablesTest, fishablesProbabilityTest);
+        assert.same(fishableRes, locInstance.fishableObjects["nemo"]);
+        
+        allowedFishablesTest = { "deadFish", "lollipop" };
+        fishablesProbabilityTest = { 0, 100 };
+        fishableRes = locInstance:determineFishable(allowedFishablesTest, fishablesProbabilityTest);
+        assert.are_not.same(fishableRes, locInstance.fishableObjects["deadFish"])
+    end)
+end)
--- src/class/SwarmFactory.lua
+++ src/class/SwarmFactory.lua
@@ -1,13 +1,6 @@
---- The class SwarmFactory creates swarms of fishable objects
+--- The class SwarmFactory creates swarms of fishable objects defined by data.lua
 
 FishableObject = require "class.FishableObject";
-
-local fishableObjects = {};
-local currentSwarm = 0;
-
-local swarmsSewer = {};
-
-local createdFishables = {};
 
 local SwarmFactory = Class {
     --- Initializes the swarm factory
@@ -20,50 +13,60 @@ local SwarmFactory = Class {
         --- Takes the fishable form the data file
         -- @param fishable The fishable
         function fishableObject(fishable) 
-            fishableObjects[fishable.name] = fishable;
+            self.fishableObjects[fishable.name] = fishable;
         end
         
         --- Takes the sewer swarms from the data file
         -- @param swarms The swarms
         function sewer(swarms) 
-            swarmsSewer = swarms;
+            self.swarmsSewer = swarms;
         end
         
         dofile("data.lua");
         addedHeights = 800; -- Start at 800 to create swarms for now
-        for i = 1, #swarmsSewer, 1 do
+        for i = 1, #self.swarmsSewer, 1 do
             self:createNextSwarm(addedHeights);
-            addedHeights = addedHeights + swarmsSewer[i].swarmHeight;
+            addedHeights = addedHeights + self.swarmsSewer[i].swarmHeight;
         end
     end;
+    
+    level = nil;
+    player = nil;
+    
+    fishableObjects = {};
+    currentSwarm = 0;
+
+    swarmsSewer = {};
+
+    createdFishables = {};
 };
 
 --- Draws all fishables
 function SwarmFactory:draw()
-    for i = 1, #createdFishables, 1 do
-        createdFishables[i]:draw();
+    for i = 1, #self.createdFishables, 1 do
+        self.createdFishables[i]:draw();
     end
 end
 
 --- Updates all fishables
 function SwarmFactory:update()
-    for i = 1, #createdFishables, 1 do
-        createdFishables[i]:update();
+    for i = 1, #self.createdFishables, 1 do
+        self.createdFishables[i]:update();
     end
 end
 
 --- Creates the next swarm
 -- @param startPosY Start y position of the swarm
 function SwarmFactory:createNextSwarm(startPosY)
-    currentSwarm = currentSwarm + 1;
-    newSwarm = swarmsSewer[currentSwarm];
+    self.currentSwarm = self.currentSwarm + 1;
+    newSwarm = self.swarmsSewer[self.currentSwarm];
     amountFishables = math.random(newSwarm.minFishables, newSwarm.maxFishables);
  
     for i = 1, amountFishables, 1 do
         yPos = math.random(newSwarm.swarmHeight);
         fishable = self:determineFishable(newSwarm.allowedFishables, newSwarm.fishablesProbability);
 
-        createdFishables[#createdFishables + 1] = FishableObject(fishable.image, startPosY + yPos,
+        self.createdFishables[#self.createdFishables + 1] = FishableObject(fishable.image, startPosY + yPos,
             fishable.minSpeed, fishable.maxSpeed, fishable.xHitbox, fishable.yHitbox,
             fishable.value, fishable.hitpoints);
     end
@@ -76,10 +79,11 @@ end
 function SwarmFactory:determineFishable(allowedFishables, fishablesProbability)
     fishableDecider = math.random(100);
     addedProbability = 0;
+    
     for i = 1, #fishablesProbability, 1 do
         addedProbability = addedProbability + fishablesProbability[i];
-        if addedProbability > fishableDecider then
-            return fishableObjects[allowedFishables[i]];
+        if addedProbability >= fishableDecider then
+            return self.fishableObjects[allowedFishables[i]];
         end
     end
 end
