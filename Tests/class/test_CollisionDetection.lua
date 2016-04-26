de4035e4d42326c49679ec750ab13188e3f2d72b
Burak Kutlu <buk5234@thi.de>
2016-04-26 15:36:14 +0200

    CollisionDetection Unittest extended and changed back

--- Tests/class/test_CollisionDetection.lua
+++ Tests/class/test_CollisionDetection.lua
@@ -6,11 +6,31 @@ describe ( "relative position", function()
     local locInstance = nil;
     
     data_collision = {
-        {1, false},
-        {2, true},
-        {3, true},
-        {4, true},
-        {5, false}
+        {1, 1, false},
+        {1, 2, false},
+        {1, 3, false},
+        {1, 4, false},
+        {1, 5, false},
+        {2, 1, false},
+        {2, 2, true},
+        {2, 3, true},
+        {2, 4, true},
+        {2, 5, false},
+        {3, 1, false},
+        {3, 2, true},
+        {3, 3, true},
+        {3, 4, true},
+        {3, 5, false},
+        {4, 1, false},
+        {4, 2, true},
+        {4, 3, true},
+        {4, 4, true},
+        {4, 5, false},
+        {5, 1, false},
+        {5, 2, false},
+        {5, 3, false},
+        {5, 4, false},
+        {5, 5, false},
     };
     
     before_each(function()
@@ -20,10 +40,15 @@ describe ( "relative position", function()
     it('testing collision calculation', function()
         for _,v in pairs(data_collision) do
             locInstance:setCollision(false);
-            locInstance:calculateCollision ( v[1], 2, 2 );
+            locInstance:calculateCollision ( v[1], v[2], 2, 2, 2, 2 );
             local result = locInstance:getCollision();
-            assert.are.equal ( v[2], result );
+            assert.are.equal ( v[3], result );
         end
+    end)
+
+    it("Testing setCollision and getCollision", function()
+        testClass:setCollision(true);
+        assert.are.same(testClass:getCollision(), true);
     end)
 
 end)
