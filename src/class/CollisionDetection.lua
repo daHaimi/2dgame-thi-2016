0e0c417f33618300bedc115ad6a49eb478678cfa
Burak Kutlu <buk5234@thi.de>
2016-05-10 16:18:59 +0200

    improved code

--- src/class/CollisionDetection.lua
+++ src/class/CollisionDetection.lua
@@ -3,8 +3,7 @@ Class = require "lib.hump.class";
 --- Class for CollisionDetection
 local CollisionDetection = Class {
     init = function(self) end;
-    collision = false;
-};
+    collision = false };
 
 --- Setter for collision
 function CollisionDetection:setCollision(newCollision)
