70d3a626f556572e625b6f4017fd2e1d7f676452
Mathias Haimerl <mathias.haimerl@thi.de>
2016-05-24 16:58:07 +0200

    Again and again

--- build.sh
+++ build.sh
@@ -96,7 +96,7 @@ cp bin/game.love tmp/assets/game.love
 rm tmp/META-INF/*.SF 2> /dev/null
 rm tmp/META-INF/*. 2> /dev/null
 sed -i 's/LÖVE for Android/2D Game THI 2016/g' tmp/AndroidManifest.xml
-sed -i 's/org.love2d.android/de.thi.projekt.ss16/g' tmp/AndroidManifest.xml
+#sed -i 's/org.love2d.android/de.thi.projekt.ss16/g' tmp/AndroidManifest.xml
 cat tmp/AndroidManifest.xml
 /usr/local/bin/apktool b tmp -o bin/${GAME_NAME}-${BUILD_NR}-android.apk -a /usr/local/bin/aapt -p frmtmp
 rm -rf tmp frmtmp
