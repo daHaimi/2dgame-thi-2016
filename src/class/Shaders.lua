8037b1fa1e74ea01191a82eeed0adeb33a938e22
Daniel Zistl <daz6290@thi.de>
2016-05-30 18:10:25 +0200

    remove redundant shaders

--- src/class/Shaders.lua
+++ src/class/Shaders.lua
@@ -2,7 +2,6 @@ Class = require "lib.hump.class";
 
 --- Class containing Funcions to add shaders
 local Shaders = Class {
-    p_glowShader = _G.love.graphics.newShader("shader/fragment/glow.glsl");
     p_hueShader = _G.love.graphics.newShader("shader/fragment/hueAdjust.glsl");
 };
 
