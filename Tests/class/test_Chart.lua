5b7cd26fe8fe338a7eb27f297f3755cd80b8ad4a
Samson Gross <sag1053@thi.de>
2016-06-01 19:56:12 +0200

    added some functions to test_chart

--- Tests/class/test_Chart.lua
+++ Tests/class/test_Chart.lua
@@ -17,6 +17,8 @@ describe("Unit test for Chart.lua", function()
         };
         
         Element = {
+            object = {
+                GetPos = function(...) return 40, 60; end;};
             visible = nil;
             x = nil;
             y = nil;
@@ -24,7 +26,6 @@ describe("Unit test for Chart.lua", function()
         function Element:SetVisible(visible) 
             self.visible = visible; 
         end;
-        
         function Element:SetPos(x, y) 
             self.x = x; 
             self.y = y; 
@@ -104,38 +105,98 @@ describe("Unit test for Chart.lua", function()
 
     it("Testing resetMarkedFrame function", function()
         locInstance:resetMarkedFrame()
-        assert.are.equal(locInstance.markFrame.visible, false);
+        assert.are.equal(locInstance.p_markFrame.visible, false);
     end)
 
+    it("Testing SetVisible function", function()
+        spy.on(locInstance, "drawChart");
+        locInstance:SetVisible(true);
+        assert.are.equal(locInstance.button_up.visible, true);
+        assert.are.equal(locInstance.button_down.visible, true);
+        assert.are.equal(locInstance.textField.objBackground.visible, true);
+        assert.are.equal(locInstance.textField.objPrice.visible, true);
+        assert.are.equal(locInstance.textField.objText.visible, true);
+        assert.are.equal(locInstance.textField.objTopic.visible, true);
+        assert.spy(locInstance.drawChart).was.called();
+        assert.are.equal(locInstance.p_markFrame.visible, false);
+        
+        locInstance.p_elementsOnChart = {Element, Element};
+        locInstance:SetVisible(false);
+        assert.are.equal(locInstance.button_up.visible, false);
+        assert.are.equal(locInstance.button_down.visible, false);
+        assert.are.equal(locInstance.textField.objBackground.visible, false);
+        assert.are.equal(locInstance.textField.objText.visible, false);
+        assert.are.equal(locInstance.textField.objTopic.visible, false);
+        assert.are.equal(locInstance.textField.objPrice.visible, false);
+        assert.are.equal(locInstance.p_elementsOnChart[1].visible, false);
+        assert.are.equal(locInstance.p_elementsOnChart[2].visible, false);
+        assert.are.equal(locInstance.p_markFrame.visible, false);
+    end)
+
+    it("Testing SetPos function", function()
+        locInstance.width = 50;
+        locInstance.klickableSize = 60;
+        spy.on(locInstance, "setPosOfKlickableElements");
+        
+        locInstance:SetPos(50, 50);
+        assert.are.equal(locInstance.p_xPos, 150);
+        assert.are.equal(locInstance.p_yPos, 50);
+        assert.are.equal(locInstance.button_up.x, 223);
+        assert.are.equal(locInstance.button_up.y, 50);
+        assert.are.equal(locInstance.button_down.x, 223);
+        assert.are.equal(locInstance.button_down.y, 140);
+        assert.spy(locInstance.setPosOfKlickableElements).was.called();
+        assert.are.equal(locInstance.textField.objBackground.x, 50);
+        assert.are.equal(locInstance.textField.objBackground.y, 503);
+    end)
+
+    it("Testing markElement function", function()
+        spy.on(locInstance.textField, "changeText");
+        
+        locInstance:markElement(Element);
+        assert.are.equal(locInstance.p_markFrame.x, 40);
+        assert.are.equal(locInstance.p_markFrame.y, 60);
+        assert.are.equal(locInstance.p_markFrame.visible, true);
+        assert.are.equal(locInstance.p_markFrame.movedToTop, true);
+        assert.are.same(locInstance.p_markedElement, Element);
+        
+        assert.spy(locInstance.textField.changeText).was_called();
+    end)
 
 --[[
+    it("Testing drawChart function", function()
+        locInstance.p_column = 3;
+        locInstance.p_toprow = 0;
+        for var = 1, 12 do
+            locInstance.p_elementsOnChart[var] = Element;
+        end
+        locInstance:drawChart()
+        for var = 1, 12 do
+            assert.are.equal(locInstance.p_elementsOnChart[var].visible, true);
+        end
+        assert.are.equal(locInstance.p_elementsOnChart[12].visible, false);
+    end)
+    
 
     it("Testing setPosOfKlickableElements function", function()
-        locInstance.p_elementsOnChart = {Element, Element, Element};
-        locInstance:setPosOfKlickableElements();
-        assert.are.equal(locInstance.p_elementsOnChart[1].x, 1);
-        assert.are.equal(locInstance.p_elementsOnChart[1].y, 1);
-        assert.are.equal(locInstance.p_elementsOnChart[2].x, 1);
-        assert.are.equal(locInstance.p_elementsOnChart[2].y, 1);
-        assert.are.equal(locInstance.p_elementsOnChart[3].x, 1);
-        assert.are.equal(locInstance.p_elementsOnChart[3].y, 1);
-        
-    end)
-
-    it("Testing drawChart function", function()
-        local testElement = {
-            visible = nil;
-        }
-        function testElement:SetVisible(visible)
-            self.visible = visible;
+        for var = 1, 4 do
+            locInstance.p_elementsOnChart[var] = Element;
         end
-        
-        locInstance.elementsOnChart = {}--{testElement, testElement, testElement};
-        locInstance.drawChart(true);
-        assert.are.equal(locInstance.elementsOnChart[1].visible, true);
-        
+        locInstance.p_xPos = 5;
+        locInstance.p_yPos = 5;
+        locInstance.p_toprow = 0;
+        locInstance.klickableSize = 90;
+        locInstance.p_column = 3;
+        locInstance.buttonHeight = 50;
+        locInstance:setPosOfKlickableElements();
+        --assert.are.equal(locInstance.p_elementsOnChart[1].x, 5);
+        --assert.are.equal(locInstance.p_elementsOnChart[1].y, 1);
+        --assert.are.equal(locInstance.p_elementsOnChart[2].x, 5);
+        --assert.are.equal(locInstance.p_elementsOnChart[2].y, 1);
+        --assert.are.equal(locInstance.p_elementsOnChart[3].x, 5);
+        --assert.are.equal(locInstance.p_elementsOnChart[3].y, 1);
+        --assert.are.equal(locInstance.p_elementsOnChart[4].x, 5);
+        --assert.are.equal(locInstance.p_elementsOnChart[4].y, 1);
     end)
 ]]--
-        
-        
 end)
