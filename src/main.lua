fc6da18cb790b427266bfea47fe8e5db2e27273e
Martin Lechner <m.lechner93@googlemail.com>
2016-06-13 19:21:26 +0200

    no error without reset

--- src/main.lua
+++ src/main.lua
@@ -57,11 +57,11 @@ function love.load()
 
     local _, _, flags = love.window.getMode();
     love.graphics.setBackgroundColor(55, 80, 100);
-    local deviceDim = { love.window.getDesktopDimensions(flags.display) };
-    --deviceDim = {720, 1080};
-    --deviceDim = {1366,768};
-    --deviceDim = {1600,900};
-    _G._persTable.winDim[1], _G._persTable.winDim[2], p_scaleFactor, titleHeight = getScaledDimension(deviceDim);
+    _G._persTable.deviceDim = { love.window.getDesktopDimensions(flags.display) };
+    --_G._persTable.deviceDim = {720, 1080};
+    --_G._persTable.deviceDim = {1366,768};
+    --_G._persTable.deviceDim = {1600,900};
+    _G._persTable.winDim[1], _G._persTable.winDim[2], p_scaleFactor, titleHeight = getScaledDimension(_G._persTable.deviceDim);
 
     _G._persTable.scaledDeviceDim = {_G._persTable.winDim[1] * p_scaleFactor, _G._persTable.winDim[2] * p_scaleFactor };
     love.window.setMode(_G._persTable.scaledDeviceDim[1], _G._persTable.scaledDeviceDim[2], 
