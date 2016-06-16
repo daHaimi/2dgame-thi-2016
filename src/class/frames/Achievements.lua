221adc2c982df8d5558f1fadf40c88bcb8130b31
Martin Lechner <m.lechner93@googlemail.com>
2016-06-16 22:26:34 +0200

    entering a screen with a chart will always show the first 3 rows first

--- src/class/frames/Achievements.lua
+++ src/class/frames/Achievements.lua
@@ -70,6 +70,7 @@ function Achievements:create()
     --onclick events for all buttons
     self.elementsOnFrame.button_back.object.OnClick = function(_)
         _gui:changeFrame(_gui:getFrames().mainMenu);
+        self.elementsOnFrame.chart.object:resetTopRow();
     end
 end
 
