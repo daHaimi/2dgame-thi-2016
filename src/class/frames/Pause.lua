0dab91be8e892b048e2a9e1110a8b1c61281f9c3
Martin Lechner <m.lechner93@googlemail.com>
2016-06-20 15:13:25 +0200

    adjusted Pause

--- src/class/frames/Pause.lua
+++ src/class/frames/Pause.lua
@@ -2,37 +2,16 @@ Class = require "lib.hump.class";
 
 local Pause = Class {
     init = function(self)
-        if _G._persTable.scaledDeviceDim[1] < 640 then
-            self.directory = "assets/gui/480px/";
-            self.widthPx = 480;
-            self.width = 384;
-            self.height = 666;
-            self.buttonHeight = 75;
-            self.buttonOffset = 15;
-            self.buttonWidth = 261;
-            self.speed = 50;
-        elseif _G._persTable.scaledDeviceDim[1] < 720 then
-            self.widthPx = 640;
-            self.directory = "assets/gui/640px/";
-            self.width = 512;
-            self.height = 888;
-            self.buttonOffset = 20;
-            self.buttonHeight = 96;
-            self.buttonWidth = 384;
-            self.speed = 60;
-        else
-            self.widthPx = 720;
-            self.directory = "assets/gui/720px/";
-            self.width = 576;
-            self.height = 1024;
-            self.buttonOffset = 30;
-            self.buttonHeight = 106;
-            self.buttonWidth = 392;
-            self.speed = 75;
-        end
+        self.background = love.graphics.newImage("assets/gui/StandardBG.png");
+        self.imageButton = love.graphics.newImage("assets/gui/button.png");
+        self.imageFlag = love.graphics.newImage("assets/gui/" .. _G.data.languages[_persTable.config.language].flagImage);
+        self.buttonHeight = self.imageButton:getHeight();
+        self.buttonWidth = self.imageButton:getWidth();
+        self.buttonXPosition = (_G._persTable.winDim[1] - self.buttonWidth) / 2;
+        self.offset = (_G._persTable.winDim[2] - self.background:getHeight())/2 + 30;
+        self.buttonDistance = 10;
         self.name = "Pause";
-        self.frame = Frame((_G._persTable.scaledDeviceDim[1] - self.width) / 2,
-            (_G._persTable.scaledDeviceDim[2] - self.height) / 2 - self.speed, "down", "down", self.speed, 0, -1500);
+        self.frame = Frame(0, 0, "down", "down", 50, 0, -1500);
         self:create();
     end;
 };
@@ -41,94 +20,61 @@ local Pause = Class {
 function Pause:create()
     --add, create and position all elements on this frame
     self.elementsOnFrame = {
-        background = {
-            object = Loveframes.Create("image");
-            x = 0;
-            y = 0;
-        };
-        button_backToGame = {
-            object = Loveframes.Create("imagebutton");
-            x = 0.16 * self.width;
-            y = self.buttonOffset;
-        };
-        button_backToMenu = {
-            object = Loveframes.Create("imagebutton");
-            x = 0.16 * self.width;
-            y = self.height - self.buttonHeight;
-        };
-        button_changeLevel = {
-            object = Loveframes.Create("imagebutton");
-            x = 0.16 * self.width;
-            y = self.buttonOffset + self.buttonHeight
-        };
-        button_restartLevel = {
-            object = Loveframes.Create("imagebutton");
-            x = 0.16 * self.width;
-            y = self.buttonOffset + 2 * self.buttonHeight;
-        };
-        button_options = {
-            object = Loveframes.Create("imagebutton");
-            x = 0.16 * self.width;
-            y = self.buttonOffset + 3 * self.buttonHeight;
-        };
+        button_backToGame = ImageButton(self.imageButton, self.buttonXPosition , 
+            self.offset, true);
+        button_backToMenu = ImageButton(self.imageButton, self.buttonXPosition , 
+            (_G._persTable.winDim[2] + self.background:getHeight())/2 - self.buttonHeight - 30, true);
+        button_changeLevel = ImageButton(self.imageButton, self.buttonXPosition ,  
+            self.offset + (self.buttonHeight + self.buttonDistance) * 1, true);
+        button_restartLevel = ImageButton(self.imageButton, self.buttonXPosition ,  
+            self.offset + (self.buttonHeight + self.buttonDistance) * 2, true);
+        button_options = ImageButton(self.imageButton, self.buttonXPosition ,  
+            self.offset + (self.buttonHeight + self.buttonDistance) * 3, true);
     };
 
-    --adjust all elements on this frame
-    self.elementsOnFrame.background.object:SetImage(self.directory .. "StandardBG.png");
-
-    self.elementsOnFrame.button_backToGame.object:SetImage(self.directory .. "Button.png")
-    self.elementsOnFrame.button_backToGame.object:SizeToImage();
-
-    self.elementsOnFrame.button_changeLevel.object:SetImage(self.directory .. "Button.png")
-    self.elementsOnFrame.button_changeLevel.object:SizeToImage();
-
-    self.elementsOnFrame.button_backToMenu.object:SetImage(self.directory .. "Button.png");
-    self.elementsOnFrame.button_backToMenu.object:SizeToImage();
-
-    self.elementsOnFrame.button_restartLevel.object:SetImage(self.directory .. "Button.png");
-    self.elementsOnFrame.button_restartLevel.object:SizeToImage();
-
-    self.elementsOnFrame.button_options.object:SetImage(self.directory .. "Button.png");
-    self.elementsOnFrame.button_options.object:SizeToImage();
-
     --onclick events for all buttons
-    self.elementsOnFrame.button_backToGame.object.OnClick = function(_)
+    self.elementsOnFrame.button_backToGame.gotClicked = function(_)
         _gui:changeFrame(_gui:getFrames().inGame);
     end
 
-    self.elementsOnFrame.button_changeLevel.object.OnClick = function(_)
+    self.elementsOnFrame.button_changeLevel.gotClicked = function(_)
         _gui:changeFrame(_gui:getFrames().level);
     end
 
-    self.elementsOnFrame.button_backToMenu.object.OnClick = function(_)
+    self.elementsOnFrame.button_backToMenu.gotClicked = function(_)
         self:checkAchRageQuit();
         _gui:getLevelManager():freeManagedObjects(); -- cleanup level, bait and swarmfactory
         _gui:changeFrame(_gui:getFrames().mainMenu);
     end
 
-    self.elementsOnFrame.button_restartLevel.object.OnClick = function(_)
+    self.elementsOnFrame.button_restartLevel.gotClicked = function(_)
         self:checkAchRageQuit();
         _gui:getLevelManager():replayLevel();
         _gui:changeFrame(_gui:getFrames().inGame);
     end
 
-    self.elementsOnFrame.button_options.object.OnClick = function(_)
+    self.elementsOnFrame.button_options.gotClicked = function(_)
         _gui:changeFrame(_gui:getFrames().options);
     end
 end
 
 --- changes the language of this frame
 function Pause:setLanguage(language)
-    self.elementsOnFrame.button_options.object:SetText(_G.data.languages[language].package.buttonOptions);
-    self.elementsOnFrame.button_restartLevel.object:SetText(_G.data.languages[language].package.buttonRestart);
-    self.elementsOnFrame.button_backToMenu.object:SetText(_G.data.languages[language].package.buttonBTM);
-    self.elementsOnFrame.button_backToGame.object:SetText(_G.data.languages[language].package.buttonBTG);
-    self.elementsOnFrame.button_changeLevel.object:SetText(_G.data.languages[language].package.buttonChangeLevel);
+    self.elementsOnFrame.button_options:setText(_G.data.languages[language].package.buttonOptions);
+    self.elementsOnFrame.button_restartLevel:setText(_G.data.languages[language].package.buttonRestart);
+    self.elementsOnFrame.button_backToMenu:setText(_G.data.languages[language].package.buttonBTM);
+    self.elementsOnFrame.button_backToGame:setText(_G.data.languages[language].package.buttonBTG);
+    self.elementsOnFrame.button_changeLevel:setText(_G.data.languages[language].package.buttonChangeLevel);
 end
 
 --- shows the frame on screen
 function Pause:draw()
-    self.frame:draw(self.elementsOnFrame);
+    local _, y = self.elementsOnFrame.button_options:getOffset();
+    love.graphics.draw(self.background, (_G._persTable.winDim[1] - self.background:getWidth())/2,
+        (_G._persTable.winDim[2] - self.background:getHeight())/2 + y);
+    for _, v in pairs (self.elementsOnFrame) do
+        v:draw();
+    end
 end
 
 --- called to "delete" this frame
@@ -157,6 +103,18 @@ function Pause:checkAchRageQuit()
     _gui:getLevelManager():getAchievmentManager():checkRageQuit(
         _gui:getLevelManager():getCurLevel():getReachedDepth());
     _gui:getLevelManager():getAchievmentManager():achBitch();
+end
+
+function Pause:mousepressed(x, y)    
+    for _, v in pairs (self.elementsOnFrame) do
+        local xPosition, yPosition = v:getPosition();
+        local width, height = v:getSize();
+        
+        if x > xPosition and x < xPosition + width and
+        y > yPosition and y < yPosition + height then
+            v.gotClicked();
+        end
+    end
 end
 
 return Pause;
