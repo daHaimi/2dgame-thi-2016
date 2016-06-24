359ea0ec7131d187182d752ed9a8f735254e49c5
Samson Gross <sag1053@thi.de>
2016-06-24 16:59:35 +0200

    added NyanCat to dictionary

--- src/data.lua
+++ src/data.lua
@@ -30,6 +30,11 @@ return {
                 }
             };
         };
+        nyan = {
+            name = "nyan";
+            image = "nyan.png";
+            sortNumber = 13;
+        };
         turtle = {
             name = "turtle";
             image = "turtle.png";
@@ -1257,6 +1262,10 @@ return {
                     description = "You got all achievements?!";
                 };
                 --objects
+                nyan = {
+                    name = "NyanCat";
+                    description = "The internet is for p... Nyan Cats";
+                };
                 nemo = {
                     name = "Nemo";
                     description = "Congratulations! You have found nemo!";
@@ -1555,6 +1564,10 @@ return {
 --                    description = "Nur ein Objekt gefangen"
 --                };
                 --objects
+                nyan = {
+                    name = "NyanCat";
+                    description = "The internet is for p... Nyan Cats";
+                };
                 nemo = {
                     name = "Nemo";
                     description = "Anscheinend hast du Nemo gefunden";
